e530a3d1e5a285ab2a4e678f902582e1d1ff7fc7
AXEL ANTONIO GARZA MARTINEZ <garza_axel@uadec.edu.mx>
2023-08-25 16:03:34 -0600

    Agregar archivos de proyecto.

---
+++ testInterop_CS/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace testInterop_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardarExcel = new System.Windows.Forms.Button();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnGuardarWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGuardarExcel
+            // 
+            this.btnGuardarExcel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarExcel.Location = new System.Drawing.Point(236, 115);
+            this.btnGuardarExcel.Name = "btnGuardarExcel";
+            this.btnGuardarExcel.Size = new System.Drawing.Size(113, 56);
+            this.btnGuardarExcel.TabIndex = 5;
+            this.btnGuardarExcel.Text = "Guardar Excel";
+            this.btnGuardarExcel.UseVisualStyleBackColor = true;
+            this.btnGuardarExcel.Click += new System.EventHandler(this.btnGuardarExcel_Click);
+            // 
+            // txtDato
+            // 
+            this.txtDato.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDato.Location = new System.Drawing.Point(118, 37);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(138, 27);
+            this.txtDato.TabIndex = 4;
+            this.txtDato.Text = "EVOLUTION";
+            this.txtDato.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnGuardarWord
+            // 
+            this.btnGuardarWord.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarWord.Location = new System.Drawing.Point(22, 115);
+            this.btnGuardarWord.Name = "btnGuardarWord";
+            this.btnGuardarWord.Size = new System.Drawing.Size(113, 56);
+            this.btnGuardarWord.TabIndex = 3;
+            this.btnGuardarWord.Text = "Guardar Word";
+            this.btnGuardarWord.UseVisualStyleBackColor = true;
+            this.btnGuardarWord.Click += new System.EventHandler(this.btnGuardarWord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 222);
+            this.Controls.Add(this.btnGuardarExcel);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.btnGuardarWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardarExcel;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnGuardarWord;
+    }
+}
+
---
+++ testInterop_CS/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace testInterop_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardarWord_Click(object sender, EventArgs e)
+        {
+          
+                SaveFileDialog dialogo = new SaveFileDialog();
+                if (dialogo.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                string ruta = dialogo.FileName;
+                var wordApp = new Word.Application();
+                wordApp.Visible = true;
+                wordApp.Documents.Add();
+                string dato = txtDato.Text;
+
+                wordApp.Selection.TypeText(dato);
+
+                wordApp.ActiveDocument.SaveAs2(ruta);
+            }
+
+        private void btnGuardarExcel_Click(object sender, EventArgs e)
+        {
+
+        SaveFileDialog dialogo = new SaveFileDialog();
+        if (dialogo.ShowDialog() != DialogResult.OK)
+        {
+            return;
+        }
+        string route = dialogo.FileName;
+        var excelApp = new Excel.Application();
+        excelApp.Visible = true;
+        Excel.Workbook workbook = excelApp.Workbooks.Add();
+        Excel.Worksheet worksheet = workbook.ActiveSheet;
+        worksheet.Cells[1, 1] = "Cajita de Texto de Prueba:";
+        worksheet.Cells[2, 1] = txtDato.Text;
+
+        workbook.SaveAs(route);
+    }
+    }
+    }
+
